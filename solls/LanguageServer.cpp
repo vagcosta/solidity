2bffca01823218efe048cbec53c9e0e4ae9d7ece
Christian Parpart <christian@parpart.family>
2021-01-06 18:04:24 +0100

    last bits before take off

--- solls/LanguageServer.cpp
+++ solls/LanguageServer.cpp
@@ -1,3 +1,20 @@
+/*
+	This file is part of solidity.
+
+	solidity is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	solidity is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
+*/
+// SPDX-License-Identifier: GPL-3.0
 #include <solls/LanguageServer.h>
 
 #include <liblsp/OutputGenerator.h>
@@ -208,6 +225,7 @@ void LanguageServer::validate(lsp::vfs::File const& _file, PublishDiagnosticsLis
 	m_sourceCodes.clear();
 	m_sourceCodes[_file.uri().substr(7)] = _file.str();
 
+	m_compilerStack.reset();
 	m_compilerStack = make_unique<CompilerStack>(bind(&LanguageServer::readFile, this, _1, _2));
 	// TODO: configure all compiler flags like in CommandLineInterface (TODO: refactor to share logic!)
 
